bcaa21d20555e58df7a869438ef6439905105d84
qinyuelong <qinyuelong.cn@gmail.com>
2019-04-11 23:47:12 +0800

    完成 day4 orm 建表 添加数据测试

---
+++ www/test.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import orm
+from models import User, Blog, Comment
+import asyncio
+
+
+
+
+# def test():
+#     yield from orm.create_pool(loop = loop, user='www-data', password='www-data', database='awesome')
+#
+#     u = User(name='Test', email='test@example.com', password='1234567890', image='about:blank')
+#
+#     yield from u.save()
+#
+#
+# for x in test():
+#     pass
+#
+
+
+
+async def test(loop):
+    await orm.create_pool(loop = loop, user='www-data', password='www-data', db='awesome')
+
+    u = User(name='Test中文', email='test22@example.com', password='1234567890', image='about:blank')
+
+    await u.save()
+    # await orm.destory_pool()
+
+loop = asyncio.get_event_loop()
+loop.run_until_complete(test(loop))
+loop.close()
