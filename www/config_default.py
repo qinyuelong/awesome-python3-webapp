dbf79f7dd20588a815890f30051a11fe514cd0d3
yuelong qin <qinyuelong.cn@gmail.com>
2019-06-03 11:13:26 +0800

    day6 添加配置文件

---
+++ www/config_default.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# ```
+# Default configurations.
+# ```
+
+
+__author__ = "qin"
+
+configs = {
+    'debug' : True,
+    'db' : {
+        'host' : '127.0.0.1',
+        'port' : 3306,
+        'user' : 'www',
+        'db' : 'awesome'
+    },
+    'session' : {
+        'secret' : 'Awesome'
+    }
+}
